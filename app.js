de80f8cc2a03310bc21937cc957c30d6b12c0610
Cyber-Jirou <yugioh.152376@outlook.jp>
2025-08-07 09:46:36 +0900

    add rest api service

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var notesRouter = require('./routes/notes');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/notes', notesRouter);
+
 
 app.use(function(req, res, next) {
   next(createError(404));
